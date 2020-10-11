7d78776f6990d811962380b4a3ff67df41d24e65
Aditya RN <arn1993@ccs.neu.edu>
2020-10-11 02:15:45 -0400

    Added website

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-var express = require('express')
-var cors = require('cors')
+var express = require("express")
 var app = express()
+var cors = require("cors")
 
 app.use(cors())
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname + "/ARApp.html");
+    res.sendFile(__dirname+"/examples/index.html")
 })
 
-app.listen(12345);
-console.log("App listening in 12345");
+app.get("/resume",function(req,res) {
+    res.sendFile(__dirname+"/resume.pdf")
+})
+
+app.get("/microsoft_certificate",function(req,res) {
+    res.sendFile(__dirname+"/Microsoft_Certificate.pdf")
+})
+
+app.listen(30000, function(){
+    console.log("server hosting...")
+})
